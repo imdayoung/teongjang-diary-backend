91b0e8e039cdd7fa732f4283daddc3e090489874
2da00 <dlekdud0102@naver.com>
2025-07-05 00:31:30 +0900

    initial commit

---
+++ src/main/java/com/imdayoung/teongjangdiary/TeongjangDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.imdayoung.teongjangdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeongjangDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeongjangDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imdayoung/teongjangdiary/TeongjangDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdayoung.teongjangdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeongjangDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
